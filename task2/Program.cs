99b86fc2bff35d8a8918e4b715de4dc22d5ee8f5
DevG00Ds <semenov.dmitriy.of@gmail.com>
2023-02-21 21:02:25 +0300

    finish task 2

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 26:
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+// int ReadNumber(string messageToUser)
+// {
+//     Console.WriteLine(messageToUser);
+//     int value = Convert.ToInt32(Console.ReadLine());
+//     return value;
+// }
+
+
+
+int inputData = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+for (; inputData > 10; count++)
+{
+    inputData = inputData / 10;
+
+}
+Console.WriteLine($"{count}");
